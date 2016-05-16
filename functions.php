1018ec1afbfa8a721489a934b13342ddae6d589a
Nathan Kleekamp <nkleekamp@gmail.com>
2016-05-16 12:46:25 -0400

    made ylai theme compatable with corona 2.0

--- functions.php
+++ functions.php
@@ -1,30 +1,60 @@
 <?php
-
-
 /**
-  * Script and style enqueue
+  * Add the CHILD_THEME_VERSION as a constant
   */
-function ylai_enqueue_scripts() {
-    wp_enqueue_script( 'corona-js', get_template_directory_uri() . '/js/dist/script.js', array(), CORONA_THEME_VERSION, true );
-    wp_enqueue_script( 'ylai-js', get_stylesheet_directory_uri() . '/js/dist/script.js', array(), '1.4.0', true );
-    wp_enqueue_script( 'addthis', 'https://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-571e80b3848f4add', array(), null, true );
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array(), CORONA_THEME_VERSION, 'all' );
-    wp_enqueue_style( 'parent-rtl-style', get_template_directory_uri() . '/rtl.css', array(), CORONA_THEME_VERSION, 'all' );
-    wp_enqueue_style( 'ylai-style', get_stylesheet_directory_uri() . '/style.css', array(), YLAI_THEME_VERSION, 'all' );
-    wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Oxygen', array(), null, 'all' );
+
+function ylai_add_constants( $constants ) {
+  $ylai_constants = array(
+    'CHILD_THEME_VERSION' => corona_get_theme_version( get_stylesheet_directory_uri() . '/version.json' ),
+  );
+
+  $constants = array_merge( $ylai_constants, $constants );
+
+  return $constants;
 }
-add_action( 'wp_enqueue_scripts', 'ylai_enqueue_scripts' );
+
+add_filter( 'corona_add_constants', 'ylai_add_constants' );
+
+
 
 
 /**
   * Child theme custom image sizes
   */
-set_post_thumbnail_size( 720, 540, TRUE);
+
+function ylai_custom_image_sizes() {
+  set_post_thumbnail_size( 720, 540, TRUE);
+  add_image_size( 'medium_large', 720, 540, TRUE );
+}
+
+add_action( 'corona_init', 'ylai_custom_image_sizes' );
+
+
+
+
+/**
+  * Script and style enqueue
+  */
+
+function ylai_enqueue_scripts() {
+    wp_enqueue_script( 'corona-js', get_template_directory_uri() . '/js/dist/script.js', array(), CORONA_THEME_VERSION, true );
+    wp_enqueue_script( 'ylai-js', get_stylesheet_directory_uri() . '/js/dist/script.js', array(), CHILD_THEME_VERSION, true );
+    wp_enqueue_script( 'addthis', 'https://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-571e80b3848f4add', array(), null, true );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array(), CORONA_THEME_VERSION, 'all' );
+    wp_enqueue_style( 'parent-rtl-style', get_template_directory_uri() . '/rtl.css', array(), CORONA_THEME_VERSION, 'all' );
+    wp_enqueue_style( 'ylai-style', get_stylesheet_directory_uri() . '/style.css', array(), CHILD_THEME_VERSION, 'all' );
+    wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Oxygen', array(), null, 'all' );
+}
+
+add_action( 'wp_enqueue_scripts', 'ylai_enqueue_scripts' );
+
+
 
 
 /**
   * Add Google Tag Manager
   */
+
 function ylai_google_tag_manager() {
   $gtm = "<!-- Google Tag Manager -->
 <noscript><iframe src='//www.googletagmanager.com/ns.html?id=GTM-P4JRP8'
@@ -37,27 +67,18 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <!-- End Google Tag Manager -->";
   echo($gtm);
 }
-add_action( 'google_tag_manager', 'ylai_google_tag_manager' );
+
+add_action( 'tha_body_top', 'ylai_google_tag_manager' );
+
+
 
 
 /**
   * Remove publication date and author from posts
   */
+
 function unhook_corona_posted_on() {
   remove_action( 'corona_posted_on', 'corona_posted_date_author' );
 }
+
 add_action( 'init', 'unhook_corona_posted_on' );
-
-
-
-function ylai_add_constant( $constants ) {
-  $ylai_constants = array(
-    'YLAI_THEME_VERSION' => corona_get_theme_version( get_stylesheet_directory_uri() . '/version.json' ),
-  );
-
-  $constants = array_merge( $ylai_constants, $constants );
-
-  return $constants;
-}
-
-add_filter( 'corona_add_constants', 'ylai_add_constant' );
