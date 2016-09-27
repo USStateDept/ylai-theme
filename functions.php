424309d153d8189194371fd2302110733d23f639
Nathan Kleekamp <nkleekamp@gmail.com>
2016-09-27 15:49:49 -0400

    added screendoor styles

--- functions.php
+++ functions.php
@@ -191,9 +191,11 @@ function ylai_enqueue_scripts() {
     wp_enqueue_script( 'corona-js', get_template_directory_uri() . '/js/dist/script.js', array(), CORONA_THEME_VERSION, true );
     wp_enqueue_script( 'ylai-js', get_stylesheet_directory_uri() . '/js/dist/script.js', array(), CHILD_THEME_VERSION, true );
     wp_enqueue_script( 'addthis', 'https://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-571e80b3848f4add', array(), null, true );
+    wp_enqueue_script( 'screendoor', '//d3q1ytufopwvkq.cloudfront.net/0/formrenderer.js', array(), null, false );
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array(), CORONA_THEME_VERSION, 'all' );
     wp_enqueue_style( 'parent-rtl-style', get_template_directory_uri() . '/rtl.css', array(), CORONA_THEME_VERSION, 'all' );
     wp_enqueue_style( 'ylai-style', get_stylesheet_directory_uri() . '/style.css', array(), CHILD_THEME_VERSION, 'all' );
+    wp_enqueue_style( 'screendoor-style', '//d3q1ytufopwvkq.cloudfront.net/0/formrenderer.css', array(), null, 'all' );
     wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Oxygen', array(), null, 'all' );
 }
 
