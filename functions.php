2a57e3e3dac990f714f4d17364e4442285a1b9c1
mrewers <marekrewers@gmail.com>
2017-05-30 10:40:15 -0400

    Adding Facebook tracking pixel code

--- functions.php
+++ functions.php
@@ -252,7 +252,31 @@ function ylai_add_twitter_widget() {
 
 add_action( 'wp_head', 'ylai_add_twitter_widget' );
 
-
+/**
+ * Inserts pixel code for ad campaign
+ */
+function add_ad_pixel(){
+  // if ( is_page('thank-you')) {
+    ?>
+    <!-- Facebook Pixel Code -->
+    <script>
+    !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+    n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+    document,'script','https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1096970680364107');
+    fbq('track', 'PageView');
+    fbq('track', 'ViewContent');
+    </script>
+    <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1096970680364107&ev=PageView&noscript=1"
+    /></noscript>
+    <!-- DO NOT MODIFY -->
+    <!-- End Facebook Pixel Code -->
+    <?php
+  // }
+}
+add_action('wp_head', 'add_ad_pixel');
 
 /**
   * Remove publication date and author from posts
